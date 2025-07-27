5a0160c2c240f9675628d08cd9aaa42563315ad9
Florence-namayanja <missflorence468@gmail.com>
2025-07-27 23:07:17 +0300

    initialising app with zustand state

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,30 +1,16 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import SearchBar from './components/SearchBar';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
-import RecipeDetails from './components/RecipeDetails';
 
 function App() {
   return (
-    <div>
+    <div className="App" style={{ maxWidth: '600px', margin: '0 auto', padding: '20px' }}>
       <h1>Recipe Sharing App</h1>
-      <nav>
-        <Link to="/">Home</Link> | <Link to="/add">Add Recipe</Link>
-      </nav>
-      
-      <Routes>
-        <Route path="/" element={<RecipeList />} />
-        <Route path="/add" element={<AddRecipeForm />} />
-        <Route path="/recipe/:id" element={<RecipeDetailsWrapper />} />
-      </Routes>
+      <SearchBar />
+      <AddRecipeForm />
+      <RecipeList />
     </div>
   );
 }
-
-// Helper wrapper to get the recipe ID from the URL
-import { useParams } from 'react-router-dom';
-const RecipeDetailsWrapper = () => {
-  const { id } = useParams();
-  return <RecipeDetails recipeId={Number(id)} />;
-};
 
 export default App;
