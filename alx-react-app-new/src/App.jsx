e2c6e213f57d043287fd08423108b44deb491630
Florence-namayanja <missflorence468@gmail.com>
2025-08-01 11:25:51 +0300

    add counter jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
 
 
 function App() {
@@ -28,7 +29,7 @@ function App() {
 
       <Footer />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-
+      <Counter />
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
