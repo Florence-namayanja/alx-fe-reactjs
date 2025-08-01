91ee3655cb0144cdbca5a4ade136574ea02e9a05
Florence-namayanja <missflorence468@gmail.com>
2025-08-01 10:58:25 +0300

    add inline styles

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent(){
+    return (
+        <main style ={{ padding: '9px', backgroundColor: 'white', color: 'black', fontSize: '15px'}}>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
