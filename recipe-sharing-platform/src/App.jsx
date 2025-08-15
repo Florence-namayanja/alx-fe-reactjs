d79764f70edbab361db34313e655c8cce4a964dd
Florence-namayanja <missflorence468@gmail.com>
2025-08-15 15:06:55 +0300

    add and homepage styles

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import HomePage from './components/Homepage'
 
 function App() {
   
@@ -10,7 +11,7 @@ function App() {
     <>
       <div>
         <h1 className='text-3x1 fontbold text-blue-500'> hi tailwind</h1>
-         
+         <HomePage/>
       </div>
     </>
   )
