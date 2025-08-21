5c87d1e2cb883f3fa08389dcf6a9717412f10fd0
Florence-namayanja <missflorence468@gmail.com>
2025-08-21 12:17:42 +0300

    add router react

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
 import recipesData from '../data.json'; // Import the JSON file
 
 export default function HomePage() {
@@ -14,9 +15,10 @@ export default function HomePage() {
       <h1 className="text-3xl font-bold mb-6 text-center">Recipe Sharing Platform</h1>
       <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {recipes.map((recipe) => (
-          <div
+          <Link 
+            to={`/recipes/${recipe.id}`} 
             key={recipe.id}
-            className="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-xl transform hover:scale-105 transition duration-300"
+            className="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-xl transform hover:scale-105 transition duration-300 block"
           >
             <img
               src={recipe.image}
@@ -27,7 +29,7 @@ export default function HomePage() {
               <h2 className="text-xl font-semibold mb-2">{recipe.title}</h2>
               <p className="text-gray-600">{recipe.summary}</p>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
